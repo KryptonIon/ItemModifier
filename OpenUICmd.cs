34846d6eb74bf229e904facd1cc794e6f55d6601
KryptonIon <47744493+KryptonIon@users.noreply.github.com>
2020-07-07 14:31:18 +0800

    Added `/imui` to force UI Buttons to show up

---
+++ OpenUICmd.cs
@@ -0,0 +1,24 @@
+﻿using ItemModifier.UI;
+using Terraria.ModLoader;
+
+namespace ItemModifier
+{
+	public class OpenUICmd : ModCommand
+	{
+		public override string Command => "imui";
+
+		public override string Description => "Force UI Buttons to show up";
+
+		public override string Usage => "/imui";
+
+		public override CommandType Type => CommandType.Chat;
+
+		public override void Action(CommandCaller caller, string input, string[] args)
+		{
+			MainInterface mainUI = ((ItemModifier)mod).MainUI;
+			mainUI.ItemModifierButton.Visible = true;
+			mainUI.NewItemButton.Visible = true;
+			mainUI.WikiButton.Visible = true;
+		}
+	}
+}
